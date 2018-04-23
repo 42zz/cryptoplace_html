f90454b5c63ca79c2f2a54755ed2b87c8a97aac9
Masaya Kawai <m@42zz.net>
2018-04-23 13:17:53 +0900

    共通部分include

--- js/main.js
+++ js/main.js
@@ -23,3 +23,16 @@ function sidebar(rootDir){
         }
     });
 }
+
+function right_side(rootDir){
+    $.ajax({
+        url: rootDir + "include/right_side.html", // 読み込むHTMLファイル
+        cache: false,
+        async: false,
+        dataType: 'html',
+        success: function(html){
+            html = html.replace(/\{\$root\}/g, rootDir); //header.htmlの{$root}を置換
+            document.write(html);
+        }
+    });
+}
