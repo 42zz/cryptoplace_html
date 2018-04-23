9607c51ea88f3f2a2303238125fc5c1731730eb9
Masaya Kawai <m@42zz.net>
2018-04-23 13:09:08 +0900

    init

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function header(rootDir){
+    $.ajax({
+        url: rootDir + "include/header.html", // 読み込むHTMLファイル
+        cache: false,
+        async: false,
+        dataType: 'html',
+        success: function(html){
+            html = html.replace(/\{\$root\}/g, rootDir); //header.htmlの{$root}を置換
+            document.write(html);
+        }
+    });
+}
+
+function sidebar(rootDir){
+    $.ajax({
+        url: rootDir + "include/sidebar.html", // 読み込むHTMLファイル
+        cache: false,
+        async: false,
+        dataType: 'html',
+        success: function(html){
+            html = html.replace(/\{\$root\}/g, rootDir); //header.htmlの{$root}を置換
+            document.write(html);
+        }
+    });
+}
